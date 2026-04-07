b4bb9dd343decb6d7df2ba6cf7401443a7a16079
somyatambi <somyatambi8@gmail.com>
2026-04-07 18:27:43 +0530

    deploy

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: '/api',
+  baseURL: 'https://datanest-icj1.onrender.com/api', // Render wala link yaha aayega
   headers: { 'Content-Type': 'application/json' }
 });
 
