1f3a9e61e1c6980fcd2c7aca8c1642075de270e3
somyatambi <somyatambi8@gmail.com>
2026-04-07 18:43:09 +0530

    Fix CORS for backend to allow Netlify requests

--- server/index.js
+++ server/index.js
@@ -15,7 +15,9 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.use(cors({
-  origin: ['http://localhost:5173', 'http://localhost:5174'],
+  origin: function (origin, callback) {
+    callback(null, true);
+  },
   credentials: true
 }));
 app.use(express.json());
